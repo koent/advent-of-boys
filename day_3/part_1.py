d167e8ce7425fc6929b7897de35d2b8838cb2633
TeyBerendschot <tey.berendschot@gmail.com>
2025-11-29 17:22:47 +0100

    submit answers

--- day_3/part_1.py
+++ day_3/part_1.py
@@ -12,10 +12,7 @@ with open(Path(__file__).parent / Path('input.txt'), 'r') as f:
 class Solution:
     
     def __init__(self, lines: list[str]):
-        self.input = self.parse_input(lines)
-        self.max_x, self.max_y = self._get_max_coordinates()
-        # self.grid_count = self._count_hits()
-        
+        self.input = self.parse_input(lines)        
         
     @staticmethod        
     def parse_input(lines):
@@ -30,40 +27,8 @@ class Solution:
             requests[id] = ((coordinates[0], coordinates[1]),(coordinates[0]+coordinates[2], coordinates[1]+coordinates[3]))
             
         return requests
-    
-    def _get_max_coordinates(self): 
-        max_x = 0
-        max_y = 0
-        
-        for request in self.input:
-            # Look at the x-coordinate
-            max_x = max(max_x, self.input[request][1][0])
-            # Look at the y-coordinate
-            max_y = max(max_y, self.input[request][1][1])
-        return max_x, max_y
-    
-    def _count_hits(self):
-        """For each point in the grid, lookn how many times it appears in a request"""
-        coordinate_count = defaultdict(int)
-        for i in tqdm(range(self.max_x), ascii=True, desc="Counting hits..."):
-            for j in range(self.max_y):
-                for request in self.input:
-                    # Check if the coordinate falls within the request
-                    if self.input[request][0][0]<=i<self.input[request][1][0] and self.input[request][0][1]<=j<self.input[request][1][1]:
-                        coordinate_count[(i,j)]+=1
-                        
-        return coordinate_count    
-    
-    def visualize(self):
-        for j in range(self.max_y+1):  
-            for i in range(self.max_x+1):
-                if (i,j) in self.grid_count.keys():
-                    print(self.grid_count[(i,j)], end="")
-                else:
-                    print(".", end="")
-            print()
             
-    def answer(self, threshold: int  = 2):
+    def answer(self):
         overlapping_squares = defaultdict(int)
         for id_1, id_2 in tqdm(combinations(self.input, 2), ascii=True, total = len(self.input)*(len(self.input)-1)//2):
             request_1=self.input[id_1]
--- day_3/part_2.py
+++ day_3/part_2.py
@@ -1,21 +1,14 @@
-from collections import defaultdict
-from itertools import combinations
 from pathlib import Path
 import re
 
-from tqdm import tqdm
-
 # Read the input
-with open(Path(__file__).parent / Path('test_input.txt'), 'r') as f:
+with open(Path(__file__).parent / Path('input.txt'), 'r') as f:
     lines = f.read().splitlines()
     
 class Solution:
     
     def __init__(self, lines: list[str]):
-        self.input = self.parse_input(lines)
-        self.max_x, self.max_y = self._get_max_coordinates()
-        # self.grid_count = self._count_hits()
-        
+        self.input = self.parse_input(lines)        
         
     @staticmethod        
     def parse_input(lines):
@@ -29,58 +22,35 @@ class Solution:
             
             requests[id] = ((coordinates[0], coordinates[1]),(coordinates[0]+coordinates[2], coordinates[1]+coordinates[3]))
             
-        return requests
+        return requests  
     
-    def _get_max_coordinates(self): 
-        max_x = 0
-        max_y = 0
-        
-        for request in self.input:
-            # Look at the x-coordinate
-            max_x = max(max_x, self.input[request][1][0])
-            # Look at the y-coordinate
-            max_y = max(max_y, self.input[request][1][1])
-        return max_x, max_y
-    
-    def _count_hits(self):
-        """For each point in the grid, lookn how many times it appears in a request"""
-        coordinate_count = defaultdict(int)
-        for i in tqdm(range(self.max_x), ascii=True, desc="Counting hits..."):
-            for j in range(self.max_y):
-                for request in self.input:
-                    # Check if the coordinate falls within the request
-                    if self.input[request][0][0]<=i<self.input[request][1][0] and self.input[request][0][1]<=j<self.input[request][1][1]:
-                        coordinate_count[(i,j)]+=1
-                        
-        return coordinate_count    
-    
-    def visualize(self):
-        for j in range(self.max_y+1):  
-            for i in range(self.max_x+1):
-                if (i,j) in self.grid_count.keys():
-                    print(self.grid_count[(i,j)], end="")
-                else:
-                    print(".", end="")
-            print()
-            
+    @staticmethod
     def _check_for_overlap(r_1,r_2):
         x_0 = max(r_1[0][0], r_2[0][0])
         x_1 = min(r_1[1][0], r_2[1][0])
-        if x_0>x_1:
+        if x_0>=x_1:
             return False
             
         
         y_0 = max(r_1[0][1], r_2[0][1])
         y_1 = min(r_1[1][1], r_2[1][1])
-        if y_0>y_1:
+        if y_0>=y_1:
             return False
         
         return True
             
     def answer(self):
+        nonoverlapping = {}
         for id_1 in self.input:
+            nonoverlapping[id_1]=True
             for id_2 in self.input:
-                self._check_for_overlap()
+                if id_1==id_2:
+                    pass
+                elif self._check_for_overlap(self.input[id_1], self.input[id_2]):
+                    nonoverlapping[id_1] = False
+        
+        # Return the id's that are nonioverlapping
+        return [k for k,v in nonoverlapping.items() if v][0]
                 
                 
 if __name__ == "__main__":
